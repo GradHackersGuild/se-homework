efb321d4e4004fdeb48277b4038bf4d3b77a5d60
Varun Varatharajan <59575040+V4run14@users.noreply.github.com>
2024-08-23 20:31:16 +0000

    added pytest suite

--- myFile.py
+++ myFile.py
@@ -1,17 +1,17 @@
 def add(a, b):
-    print("Sum of two numbers is ", a+b)
+    return(a+b)
 
 def subtract(a, b):
-    print("Difference of two numbers is ", a-b)
+    return(a-b)
 
 def multiply(a, b):
-    print("Product of two numbers is ", a*b)
+    return(a*b)
 
 def divide(a, b):
-    print("Quotient of two numbers is ", a/b)
+    return(a/b)
 
 a, b = 3, 1
-add(a, b)
-subtract(a, b)
-multiply(a, b)
-divide(a, b)
+print(add(a, b))
+print(subtract(a, b))
+print(multiply(a, b))
+print(divide(a, b))
