e2d2d2b46bc4c1afa2c21431bfc3bc83b844b9a6
Varun Varatharajan <59575040+V4run14@users.noreply.github.com>
2024-08-23 19:28:33 +0000

    Initial steps commit

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,3 @@
-### Hello World
+The MIT License
+License: MIT
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
---
+++ myFile.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    print("Sum of two numbers is ", a+b)
+
+def subtract(a, b):
+    print("Difference of two numbers is ", a-b)
+
+def multiply(a, b):
+    print("Product of two numbers is ", a*b)
+
+def divide(a, b):
+    print("Quotient of two numbers is ", a/b)
+
+a, b = 3, 1
+add(a, b)
+subtract(a, b)
+multiply(a, b)
+divide(a, b)
