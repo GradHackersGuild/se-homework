fe9e867bbd630296892efbc6f47c3c0f82fa244a
MakarandPundlik <65530539+MakarandPundlik@users.noreply.github.com>
2024-08-24 13:28:59 +0000

    initial commit

---
+++ maths.py
@@ -0,0 +1,18 @@
+def division(x,y):
+    return x/y
+
+def multiplication(x,y):
+    return x*y
+    
+def addition(x,y):
+    return x+y
+   
+def subtraction(x,y):
+    return x-y
+
+
+
+# multiplication(10,3)
+# addition(7,2)
+# subtraction(67,-543)
+# division(45,0)
