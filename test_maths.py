950c312582b78c1518f6afd8ebd09d1f65151eeb
MakarandPundlik <mmp2308@gmail.com>
2024-08-27 15:12:58 -0400

    added appropriate results

--- test_maths.py
+++ test_maths.py
@@ -5,8 +5,4 @@ def test_addition():
     assert OPs.addition(5,3) == 8
 
 def test_multiplication():
-    assert OPs.multiplication(6,6) == 36
-
-#pytest==8.3.2
-# pytest-cov==5.0.0
-#coverage==7.6.1
+    assert OPs.multiplication(6,-6) == 36
