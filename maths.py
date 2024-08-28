e1767c0a608c4d88c484e2ed80af40d6013e211c
michellevarghese <73420769+michellevarghese@users.noreply.github.com>
2024-08-28 23:48:50 +0000

    Refactored division function

--- maths.py
+++ maths.py
@@ -1,8 +1,5 @@
 import math as Math
 
-def division(x,y):
-    return x/y
-
 def multiplication(x,y):
     return x*y
     
@@ -18,8 +15,10 @@ def power(x,y):
 def modulus(x,y):
     return x%y
 
+def division(x,y):
+    return x/y
 
+# division(45,0)
 # multiplication(10,3)
 # addition(7,2)
 # subtraction(67,-543)
-# division(45,0)
